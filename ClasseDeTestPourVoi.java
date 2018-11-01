dbcdb057012d00ce3675fb0cb1d69d0bd595d36b
nicoapps <33573107+nicoapps@users.noreply.github.com>
2018-11-01 21:54:43 +0100

    Update ClasseDeTestPourVoi.java

---
+++ ClasseDeTestPourVoi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lepyn
+ */
+public class ClasseDeTestPourVoi {
+    
+    private int _unNombre;
+    
+    public ClasseDeTestPourVoi()
+    {
+        _unNombre = 13;
+    }
+}
